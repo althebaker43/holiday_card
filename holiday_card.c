e833b882653e7187d2bf36f3e51881fa09a62053
Allen Baker <althebaker43@gmail.com>
2019-04-27 17:01:05 -0700

    Began implementing auto-playing sequences of notes.

--- holiday_card.c
+++ holiday_card.c
@@ -25,6 +25,36 @@
 #define PRESCALE 1
 #define FREQ_TO_COUNT(freq) ((CLK_IO / (2 * freq * PRESCALE)) - 1)
 
+#define BEAT_DIV 3
+#define SEQ_PRESCALE 1024
+#define BPM_TO_COUNT(bpm) ((CLK_IO * (uint32_t)60) / (2 * BEAT_DIV * (uint32_t)(SEQ_PRESCALE + 1) * bpm))
+
+#define MAX_NUM_NOTES 16
+struct Note {
+  uint8_t code;
+  uint8_t count;
+};
+struct Note Notes [MAX_NUM_NOTES];
+static unsigned int CurrentNoteIdx = 0;
+#define END_SEQ {0, 0}
+
+enum Codes {
+  CODE_NONE = 0,
+  CODE_A3,
+  CODE_Bflat,
+  CODE_B,
+  CODE_C,
+  CODE_Csharp,
+  CODE_D,
+  CODE_Dsharp,
+  CODE_E,
+  CODE_F,
+  CODE_Fsharp,
+  CODE_G,
+  CODE_Gsharp,
+  CODE_A4
+};
+
 
 void toneTimerSetup()
 {
@@ -143,6 +173,23 @@ void toggleLEDs(unsigned int keyCode)
     }
 }
 
+void enableKeyInterrupts()
+{
+  // Enable interrupts on PCINT16-19
+  set(PCMSK2, PCINT16);
+  set(PCMSK2, PCINT17);
+  set(PCMSK2, PCINT18);
+  set(PCMSK2, PCINT19);
+}
+
+void disableKeyInterrupts()
+{
+  clear(PCMSK2, PCINT16);
+  clear(PCMSK2, PCINT17);
+  clear(PCMSK2, PCINT18);
+  clear(PCMSK2, PCINT19);
+}
+
 void keySetup()
 {
   // Enable pull-ups on PD0-3
@@ -151,11 +198,7 @@ void keySetup()
   set(PORTD, PORTD2);
   set(PORTD, PORTD3);
 
-  // Enable interrupts on PCINT16-19
-  set(PCMSK2, PCINT16);
-  set(PCMSK2, PCINT17);
-  set(PCMSK2, PCINT18);
-  set(PCMSK2, PCINT19);
+  enableKeyInterrupts();
 
   // Enable interrupts on any pin change
   set(PCICR, PCIE2);
@@ -203,6 +246,77 @@ ISR(PCINT2_vect)
   reti();
 }
 
+void sequenceTimerSetup()
+{
+  // Clear timer 0 on compare match
+  set(TCCR0A, WGM01);
+
+  // Set timer 0 output on compare match
+  /* set(TCCR0A, COM0A0); */
+  /* set(TCCR0A, COM0A1); */
+
+  // Enable interrupts on compare match
+  set(TIMSK0, OCIE0A);
+
+  // Set timer 0 compare value
+  OCR0A = (uint8_t)BPM_TO_COUNT(100);
+}
+
+void initSequence(struct Note sequence[])
+{
+  disableKeyInterrupts();
+
+  unsigned int initNoteIdx = 0;
+  for (; sequence[initNoteIdx].count == 0; ++initNoteIdx)
+    {
+      Notes[initNoteIdx] = sequence[initNoteIdx];
+    }
+
+  CurrentNoteIdx = 0;
+
+  tone(Notes[CurrentNoteIdx].code);
+  toggleLEDs(Notes[CurrentNoteIdx].code);
+
+  // Clear timer 0 count
+  TCNT0 = 0;
+
+  // Activate timer 0 (with prescaler set to 1024)
+  TCCR0B |= ((1 << CS00 ) | (1 << CS02));
+}
+
+void onBeatDiv()
+{
+  if (Notes[CurrentNoteIdx].count > 0)
+    {
+      --(Notes[CurrentNoteIdx].count);
+      return;
+    }
+
+  ++CurrentNoteIdx;
+
+  if (Notes[CurrentNoteIdx].count == 0)
+    {
+      // Deactivate timer 0
+      TCCR0B &= ~((1 << CS00) | (1 << CS01) | (1 << CS02));
+
+      tone(0);
+      toggleLEDs(0);
+
+      enableKeyInterrupts();
+
+      return;
+    }
+
+  tone(Notes[CurrentNoteIdx].code);
+  toggleLEDs(Notes[CurrentNoteIdx].code);
+}
+
+ISR(TIMER0_COMPA_vect)
+{
+  onBeatDiv();
+  reti();
+}
+
 int main()
 {
   cli();
@@ -213,7 +327,11 @@ int main()
 
   ledSetup();
 
+  sequenceTimerSetup();
+
   sei();
+
+  //initSequence((struct Note[]){{1 << 0, 1}, {1 << 1, 1}, {1 << 2, 1}, {1 << 3, 1}, END_SEQ});
 
   sleep_mode();
 
